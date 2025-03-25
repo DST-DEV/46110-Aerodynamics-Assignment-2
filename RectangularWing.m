8c0e9acd97189568fbcb155a3959198c5ba4c171
Davis Stöwer <davis.stoewer@gmail.com>
2025-03-25 19:33:23 +0100

    Added non-uniform y-coordinate option to rectangular wing

--- RectangularWing.m
+++ RectangularWing.m
@@ -25,7 +25,7 @@ classdef RectangularWing
             obj.c_root = obj.S / obj.b;
         end
         
-        function [y, theta] = generate_coordinates(obj, N)
+        function [y, theta] = generate_coordinates(obj, N, density_factor)
             % generate_coordinates Generate spanwise coordinates.
             %   [y, theta] = generate_coordinates(obj, N) generates the spanwise
             %   coordinates (y) and corresponding angular coordinates (theta) for
@@ -39,11 +39,30 @@ classdef RectangularWing
             %       theta - Angular coordinates
 
 
-            theta = linspace(0, pi, N+2);
-            theta = theta(2:end-1);  % Remove the first and last point to avoid singularities
-
+            % theta = linspace(0, pi, N+2);
+            % theta = theta(2:end-1);  % Remove the first and last point to avoid singularities
             %Calculate spanwise coordinates
-            y = -obj.b/2 * cos(theta);   % Spanwise locations
+            % y = -obj.b/2 * cos(theta);   % Spanwise locations
+
+            if nargin<3
+                density_factor = 0;
+            end
+
+            % density_factor = 1 + 1/(density_factor-.6)^2;
+            if density_factor == 0
+                theta = linspace(0, pi, N+2);
+                theta = theta(2:end-1);  % Remove the first and last point to avoid singularities
+
+                % Calculate spanwise coordinates
+                y = -obj.b/2 * cos(theta);
+            else
+                x = linspace(-5, 5, N+2);
+                y0 = (sqrt(4*density_factor.^2*x.^2 +1) - 1)./(2*x);
+                y1 = y0./(max(abs(y0)));
+                y1 = y1(2:end-1);
+                y = y1*obj.b/2;
+                theta = acos(-y1);
+            end
         end
         
         function c = chord_length(obj, y)
